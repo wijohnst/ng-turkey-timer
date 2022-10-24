f8604efb0a2928968374bb8b85c58ca8adaf2d92
will-johnston-goodyear <will_johnston@goodyear.com>
2022-10-24 10:44:11 -0400

    Adds LeafTimer component

---
+++ src/app/leaf-timer/leaf-timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leaf-timer',
+  templateUrl: './leaf-timer.component.html',
+  styleUrls: ['./leaf-timer.component.css']
+})
+export class LeafTimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
