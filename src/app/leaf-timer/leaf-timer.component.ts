d79d721a5d0a062bb4f6059cd5c8c55668e15726
will-johnston-goodyear <will_johnston@goodyear.com>
2022-10-24 10:55:01 -0400

    Adds simple styling

--- src/app/leaf-timer/leaf-timer.component.ts
+++ src/app/leaf-timer/leaf-timer.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-leaf-timer',
-  templateUrl: './leaf-timer.component.html',
-  styleUrls: ['./leaf-timer.component.css']
+  selector: "app-leaf-timer",
+  templateUrl: "./leaf-timer.component.html",
+  styleUrls: ["./leaf-timer.component.css"],
 })
 export class LeafTimerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  isLessThanOneMonth: boolean = false;
+  moreThanOneMonthText: string =
+    "Your meal is more than a month away. Relax for now and check back in 12 days.";
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
