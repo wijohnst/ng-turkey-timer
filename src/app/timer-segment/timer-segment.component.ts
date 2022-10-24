1031b2061ceb07afb858203e7315d19ea65c835a
will-johnston-goodyear <will_johnston@goodyear.com>
2022-10-24 10:40:37 -0400

    Updates dynamic styling for timer segments

--- src/app/timer-segment/timer-segment.component.ts
+++ src/app/timer-segment/timer-segment.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from "@angular/core";
+import { Component, OnInit, Input, SimpleChanges } from "@angular/core";
 
 type ValuesTuple = [number, number];
 
@@ -24,33 +18,11 @@ export type LeafConfig = {
   templateUrl: "./timer-segment.component.html",
   styleUrls: ["./timer-segment.component.css"],
 })
-export class TimerSegmentComponent implements OnInit, OnChanges {
+export class TimerSegmentComponent implements OnInit {
   constructor() {}
 
   @Input() values: ValuesTuple = [0, 0];
   @Input() durationString: DurationStrings = DurationStrings.DAYS;
-  @Input() leafConfig: LeafConfig = {
-    numberSize: "2rem",
-    dynamicWrapperSize: "200px",
-  };
-  @Input() durationStringSize: string = "1rem";
 
-  durationStringStyle = {
-    "font-size": this.durationStringSize,
-  };
-
-  localLeafConfig;
-
-  ngOnInit() {
-    this.durationStringStyle["font-size"] = this.durationStringSize;
-    this.localLeafConfig = this.leafConfig;
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
-    this.durationStringStyle["font-size"] =
-      changes.durationStringSize.currentValue;
-
-    this.localLeafConfig = { ...changes.leafConfig.currentValue };
-  }
+  ngOnInit() {}
 }
--- src/app/track-meal/track-meal.component.ts
+++ src/app/track-meal/track-meal.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
 import { TimerService } from "../timer.service";
-import { BreakpointObserverService } from "../breakpoint-observer.service";
-import { Breakpoints } from "@angular/cdk/layout";
-import { LeafConfig } from "../timer-segment/timer-segment.component";
 
 @Component({
   selector: "app-track-meal",
@@ -11,22 +8,10 @@ import { LeafConfig } from "../timer-segment/timer-segment.component";
   styleUrls: ["./track-meal.component.css"],
 })
 export class TrackMealComponent implements OnInit, OnDestroy {
-  constructor(
-    private timerService: TimerService,
-    private breakpointObserverService: BreakpointObserverService
-  ) {}
+  constructor(private timerService: TimerService) {}
 
   timeToServiceSubscription: Subscription;
   timeToService: Duration = this.timerService.getTimeToService();
-
-  // Dynamic Styling
-  Breakpoints = Breakpoints;
-  breakpointObserverServiceSubscription: Subscription;
-  leafConfig: LeafConfig = {
-    numberSize: "2rem",
-    dynamicWrapperSize: "200px",
-  };
-  durationStringSize: string = "8rem";
 
   ngOnInit() {
     this.timeToServiceSubscription = this.timerService
@@ -34,43 +19,11 @@ export class TrackMealComponent implements OnInit, OnDestroy {
       .subscribe(
         (timeToService: Duration) => (this.timeToService = timeToService)
       );
-
-    this.breakpointObserverServiceSubscription =
-      this.breakpointObserverService.breakpoint$.subscribe(() =>
-        this.breakpointChanged()
-      );
   }
 
   ngOnDestroy() {
     if (this.timeToServiceSubscription) {
       this.timeToServiceSubscription.unsubscribe();
-    }
-    if (this.breakpointObserverServiceSubscription) {
-      this.breakpointObserverServiceSubscription.unsubscribe();
-    }
-  }
-
-  /*
-		! Temporary implementation. Update as needed to dynamically render components based on future work.
-	*/
-  breakpointChanged() {
-    console.log("Breakpoint updated...");
-    if (
-      this.breakpointObserverService.breakpointObserver.isMatched(
-        Breakpoints.XSmall
-      )
-    ) {
-      this.leafConfig = {
-        numberSize: "1rem",
-        dynamicWrapperSize: "10px",
-      };
-      this.durationStringSize = "1rem";
-    } else {
-      this.leafConfig = {
-        numberSize: "2rem",
-        dynamicWrapperSize: "200px",
-      };
-      this.durationStringSize = "2rem";
     }
   }
 }
