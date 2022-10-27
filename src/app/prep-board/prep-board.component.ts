310e541902f73ed89aacd6e437846a11536f20be
will-johnston-goodyear <will_johnston@goodyear.com>
2022-10-27 15:32:13 -0400

    Refctors to use service.

--- src/app/prep-board/prep-board.component.ts
+++ src/app/prep-board/prep-board.component.ts
@@ -6,6 +6,7 @@ import { Recipe, PrepListEntry } from "../mealApi/recipe";
 import { TimerService } from "../timer.service";
 import { Subscription } from "rxjs";
 import { convertDurationToMinutes } from "./prep-board.utils";
+import { PrepBoardService } from "../prep-board.service";
 
 const EmptyPrepList = {
   current: [],
@@ -21,7 +22,8 @@ export class PrepBoardComponent implements OnInit, OnDestroy {
   public prepCardTypes = PrepCardType;
   constructor(
     private localStorageService: LocalStorageService,
-    private timerService: TimerService
+    private timerService: TimerService,
+    private prepBoardService: PrepBoardService
   ) {}
 
   timeToServiceSubscription: Subscription;
@@ -34,49 +36,15 @@ export class PrepBoardComponent implements OnInit, OnDestroy {
     this.mealData = this.localStorageService.getLocalMeal();
     this.timeToServiceSubscription = this.timerService
       .getTimerValues()
-      .subscribe((timeToService: Duration) => this.getPrepList(timeToService));
+      .subscribe(
+        (timeToService: Duration) =>
+          (this.prepList = this.prepBoardService.getPrepList(timeToService))
+      );
   }
 
   ngOnDestroy(): void {
     if (this.timeToServiceSubscription) {
       this.timeToServiceSubscription.unsubscribe();
     }
-  }
-
-  getPrepList(timeToService: Duration) {
-    const allPrepListEntries: PrepListEntry[] = this.mealData.recipes.flatMap(
-      (recipeData) =>
-        new Recipe(
-          recipeData.name,
-          recipeData.leadTime,
-          recipeData.prepTime
-        ).getPrepListEntries()
-    );
-
-    // Resets local values before updating
-    this.prepList.current = [];
-    this.prepList.upcoming = [];
-    this.prepList.completed = [];
-
-    const minutesToService = convertDurationToMinutes(timeToService);
-
-    allPrepListEntries.forEach((prepListEntry: PrepListEntry) => {
-      const minutesToStartRecipe = convertDurationToMinutes(
-        prepListEntry.duration
-      );
-      const startTimeDifference = minutesToService - minutesToStartRecipe;
-
-      if (startTimeDifference <= 0) {
-        this.prepList.completed.push(prepListEntry);
-        return;
-      }
-      if (startTimeDifference <= 29) {
-        this.prepList.current.push(prepListEntry);
-        return;
-      } else {
-        this.prepList.upcoming.push(prepListEntry);
-        return;
-      }
-    });
   }
 }
